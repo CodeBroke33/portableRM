91262da84055a240bd3a66c828720a9729ee7d3d
CodeBroke33 <minedom33@gmail.com>
2024-09-12 13:04:11 +0200

    still needs customisation and refinement

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,65 +4,110 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
-import { 
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt }  from "react-icons/fa"
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const info = [
-    {
-        icon: <FaPhoneAlt />,
-        title: "Phone",
-        description: "(+27)68 373 5646"
-    },
-    {
-        icon: <FaEnvelope />,
-        title: "Email",
-        description: "razienmackenzie@gmail.com" 
-    },
-    {
-        icon: <FaMapMarkerAlt />,
-        title: "Address",
-        description: "14 Tagus RdManenberg, Cape Town, 7767"    
-
-    }
-
-]
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+27)68 373 5646",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "razienmackenzie@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "14 Tagus Rd Manenberg, Cape Town, 7767",
+  },
+];
 
 import { motion } from "framer-motion";
 
-const Contact  = () => {
-    return (
-        <motion.section 
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"} 
-    }}
-    className="py-6"
+const Contact = () => {
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
     >
-        <div  className="container mx-auto">
-            <div className="flex flex-col xl:flex-row gap-[30px]">
-                {/* form */}
-                <div className="xl:h-[54%] order-2 xl:order-none">
-                    <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-                        <h3 className="text-4xl text-accent">Let's work together</h3>
-                        <p className="text-white/60">I'm always looking for opportunities to collaborate on projects that push boundaries and bring unique ideas to life. Whether you're in need of full-stack development, creative UI design, or optimizing digital experiences, let's create something exceptional together. Reach out, and let's build the future.
-
-</p>
-                    </form>
-                </div>
-                {/* info */}
-                <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
-            </div>
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form
+              action=""
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+            >
+              <h3 className="text-4xl text-accent">Let's Connect</h3>
+              <p className="text-white/60">
+              I'm always eager to collaborate on innovative projects. Whether it's full-stack development, creative UI design, or refining digital experiences, let's create something exceptional together.
+              </p>
+              {/* input */}
+              <div className="grid grid-col-1 md:grid-cols-2 gap-6 ">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a service3" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>Select a service</SelectLabel>
+                        <SelectItem value="est">Web Development</SelectItem>
+                        <SelectItem value="cst">UI&UX Design</SelectItem>
+                        <SelectItem value="mst">Logo Design</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* textarea */}
+              <Textarea
+                className="h-[100px]"
+                placeholder="Type your message here" 
+              />
+                {/* btn */}
+                <Button size="md" className="max-w-40">Send Message</Button>
+            </form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10 ">
+                {info.map((item, index)=>{
+                    return <li key={index} className="flex item-center gap-6">
+                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                            <div className="text-[28px] ">{item.icon}</div>
+                        </div>
+                        <div className="flex-1">
+                            <p className="text-white/60">{item.title}</p>
+                            <h3 className="text-xl">{item.description}</h3>
+                        </div>
+                    </li>
+                })}
+            </ul>
+          </div>
         </div>
+      </div>
     </motion.section>
-    )
-}
+  );
+};
 
-export default Contact
+export default Contact;
